1a47a72a2af68acf82068ffbb9fb524177cfb28c
Shodiya Folorunsho <folushoayomide11@gmail.com>
2022-08-14 13:56:39 +0100

    Made some changes

--- practice.js
+++ practice.js
@@ -1,4 +1,35 @@
-const test = ["damilola"];
+var calPoints = function (ops){
+    var results = null;
+    var records = [];
 
+    ops.forEach(operation => {
+        if(/^[a-zA-Z]+$/.test(operation)){
+            switch(operation){
+                case "C":
+                    records.pop();
+                break;
+                case "D":
+                    console.log(records);
+                    records = [...records, ...records.splice((records.length - 1), 1, records[records.length - 1] * 2)];
+                break;
+                default: 
+                throw new Error();    
+            }
+        }else{
+            if(operation === "+"){
+                records.push(records[records.length - 1] + records[records.length - 2]);
+            }else{
+                records.push(Number(operation));
+            }
+        }
+    })
 
-console.log(test.slice(0, 4));
+    results = records.reduce((accumulator, record) => {
+        accumulator = accumulator + record;
+        return accumulator;
+    }, 0);
+
+    return results;
+}
+
+console.log(calPoints(["5", "2", "C", "D", "+"]));
